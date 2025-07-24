3d13c76f023dda04f7d9acd49de936de38f16728
akinolaogunlana <142597105+akinolaogunlana@users.noreply.github.com>
2025-07-24 18:23:25 +0100

    Update FlashLoanArbitrage.sol

--- contracts/FlashLoanArbitrage.sol
+++ contracts/FlashLoanArbitrage.sol
@@ -1,99 +1,127 @@
-// SPDX-License-Identifier: MIT
-pragma solidity ^0.8.20;
+// SPDX-License-Identifier: MIT pragma solidity ^0.8.20;
 
-import "@aave/core-v3/contracts/flashloan/base/FlashLoanSimpleReceiverBase.sol";
-import "@aave/core-v3/contracts/interfaces/IPoolAddressesProvider.sol";
-import "@openzeppelin/contracts/token/ERC20/IERC20.sol";
-import "@openzeppelin/contracts/access/Ownable.sol";
+import "@openzeppelin/contracts/token/ERC20/IERC20.sol"; import "@openzeppelin/contracts/token/ERC20/utils/SafeERC20.sol"; import "@chainlink/contracts/src/v0.8/interfaces/AggregatorV3Interface.sol"; import "@aave/core-v3/contracts/interfaces/IPool.sol"; import "@uniswap/v2-periphery/contracts/interfaces/IUniswapV2Router02.sol";
 
-interface IUniswapV2Router {
-    function swapExactTokensForTokens(
-        uint amountIn,
-        uint amountOutMin,
-        address[] calldata path,
-        address to,
-        uint deadline
-    ) external returns (uint[] memory amounts);
+contract FlashLoanArbitrage { using SafeERC20 for IERC20;
+
+address public owner;
+IPool public lendingPool;
+IUniswapV2Router02 public router1;
+IUniswapV2Router02 public router2;
+
+address public token0; // Example: WETH
+address public token1; // Example: USDC
+
+AggregatorV3Interface internal priceFeedETHUSD;
+AggregatorV3Interface internal priceFeedUSDCUSD;
+
+constructor(
+    address _lendingPool,
+    address _router1,
+    address _router2,
+    address _token0,
+    address _token1
+) {
+    owner = msg.sender;
+    lendingPool = IPool(_lendingPool);
+    router1 = IUniswapV2Router02(_router1);
+    router2 = IUniswapV2Router02(_router2);
+    token0 = _token0;
+    token1 = _token1;
+
+    priceFeedETHUSD = AggregatorV3Interface(0x639Fe6ab55C921f74e7fac1ee960C0B6293ba612);
+    priceFeedUSDCUSD = AggregatorV3Interface(0xAed0c38402a5d19df6E4c03F4E2DceD6e29c1ee9);
 }
 
-contract FlashLoanArbitrage is FlashLoanSimpleReceiverBase, Ownable {
-    address public tokenA;
-    address public tokenB;
-    address public dex1;
-    address public dex2;
+function executeArbitrage(uint256 amount) external {
+    require(msg.sender == owner, "Only owner");
+    address ;
+    assets ;
+    amounts ;
+    modes[0] = 0; // No debt (flash loan)
 
-    constructor(
-        address _provider,
-        address _tokenA,
-        address _tokenB,
-        address _dex1,
-        address _dex2
-    ) FlashLoanSimpleReceiverBase(IPoolAddressesProvider(_provider)) {
-        tokenA = _tokenA;
-        tokenB = _tokenB;
-        dex1 = _dex1;
-        dex2 = _dex2;
-    }
-
-    function executeOperation(
-        address asset,
-        uint256 amount,
-        uint256 premium,
-        address initiator,
-        bytes calldata
-    ) external override returns (bool) {
-        require(msg.sender == address(POOL), "Only Aave pool can call");
-        require(initiator == address(this), "Unauthorized");
-
-        // Approve tokens for both routers
-        IERC20(asset).approve(dex1, amount);
-        IERC20(tokenB).approve(dex2, type(uint256).max);
-
-        address ;
-        path1[0] = tokenA;
-        path1[1] = tokenB;
-
-        address ;
-        path2[0] = tokenB;
-        path2[1] = tokenA;
-
-        // Swap on DEX1
-        uint[] memory output1 = IUniswapV2Router(dex1).swapExactTokensForTokens(
-            amount,
-            1,
-            path1,
-            address(this),
-            block.timestamp
-        );
-
-        // Swap back on DEX2
-        uint[] memory output2 = IUniswapV2Router(dex2).swapExactTokensForTokens(
-            output1[1],
-            1,
-            path2,
-            address(this),
-            block.timestamp
-        );
-
-        uint256 totalOwed = amount + premium;
-        require(output2[1] > totalOwed, "No arbitrage profit");
-
-        // Repay flash loan
-        IERC20(asset).approve(address(POOL), totalOwed);
-
-        // Keep the profit
-        uint256 profit = output2[1] - totalOwed;
-        IERC20(asset).transfer(owner(), profit);
-
-        return true;
-    }
-
-    function initiateFlashLoan(address _token, uint256 _amount) external onlyOwner {
-        POOL.flashLoanSimple(address(this), _token, _amount, "", 0);
-    }
-
-    function rescueFunds(address _token) external onlyOwner {
-        uint256 bal = IERC20(_token).balanceOf(address(this));
-        require(IERC20(_token).transfer(msg.sender, bal), "Transfer failed");
-    }
+    lendingPool.flashLoan(
+        address(this),
+        assets,
+        amounts,
+        modes,
+        address(this),
+        bytes(""),
+        0
+    );
 }
+
+function executeOperation(
+    address[] calldata assets,
+    uint256[] calldata amounts,
+    uint256[] calldata premiums,
+    address initiator,
+    bytes calldata params
+) external returns (bool) {
+    require(msg.sender == address(lendingPool), "Not LendingPool");
+    require(initiator == address(this), "Invalid initiator");
+
+    IERC20(token0).approve(address(router1), amounts[0]);
+
+    address[] memory path1 = new address[](2);
+    path1[0] = token0;
+    path1[1] = token1;
+
+    uint[] memory amountOut1 = router1.swapExactTokensForTokens(
+        amounts[0],
+        1,
+        path1,
+        address(this),
+        block.timestamp
+    );
+
+    IERC20(token1).approve(address(router2), amountOut1[1]);
+
+    address[] memory path2 = new address[](2);
+    path2[0] = token1;
+    path2[1] = token0;
+
+    uint[] memory amountOut2 = router2.swapExactTokensForTokens(
+        amountOut1[1],
+        1,
+        path2,
+        address(this),
+        block.timestamp
+    );
+
+    require(
+        isProfitable(amounts[0], amountOut1[1]),
+        "Not profitable via Chainlink"
+    );
+
+    uint256 totalOwed = amounts[0] + premiums[0];
+    IERC20(token0).approve(address(lendingPool), totalOwed);
+
+    return true;
+}
+
+function getLatestPrice(AggregatorV3Interface feed) internal view returns (int256) {
+    (, int256 price, , , ) = feed.latestRoundData();
+    return price;
+}
+
+function isProfitable(uint256 inputEth, uint256 outputUSDC) internal view returns (bool) {
+    int256 ethPrice = getLatestPrice(priceFeedETHUSD);
+    int256 usdcPrice = getLatestPrice(priceFeedUSDCUSD);
+
+    uint256 inputValueUSD = inputEth * uint256(ethPrice) / 1e8;
+    uint256 outputValueUSD = outputUSDC * uint256(usdcPrice) / 1e8;
+
+    return outputValueUSD >= (inputValueUSD * 1001) / 1000;
+}
+
+function withdraw(address token) external {
+    require(msg.sender == owner, "Only owner");
+    uint256 bal = IERC20(token).balanceOf(address(this));
+    IERC20(token).safeTransfer(owner, bal);
+}
+
+receive() external payable {}
+
+}
+
