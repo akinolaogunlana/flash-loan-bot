2fec259aa96064f347d69aa179286a8894dbae8a
akinolaogunlana <142597105+akinolaogunlana@users.noreply.github.com>
2025-07-24 17:56:03 +0100

    Create runArbitrage.js

---
+++ scripts/runArbitrage.js
@@ -0,0 +1,20 @@
+// scripts/runArbitrage.js
+const hre = require("hardhat");
+
+async function main() {
+  const contractAddress = "YOUR_DEPLOYED_CONTRACT_ADDRESS";
+  const contract = await hre.ethers.getContractAt("FlashLoanArbitrage", contractAddress);
+
+  const token = "TOKEN_ADDRESS"; // Token to borrow
+  const amount = hre.ethers.utils.parseUnits("1000", 18); // Borrow 1000 units
+
+  const tx = await contract.initiateFlashLoan(token, amount);
+  console.log("Arbitrage transaction sent:", tx.hash);
+  await tx.wait();
+  console.log("Transaction confirmed!");
+}
+
+main().catch((error) => {
+  console.error(error);
+  process.exitCode = 1;
+});
